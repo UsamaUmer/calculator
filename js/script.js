61af160a7f81a299d71eee92f327e4a074000b9b
UsamaUmer <usamaumer41407@gmail.com>
2025-12-22 20:56:19 +0500

    Workign fine AC and DEL button

--- js/script.js
+++ js/script.js
@@ -19,10 +19,34 @@ function handleClick(){
     else if(value === "DEL"){
         handleDelete();
     }
+    else if (!isNaN(value) || value === ".") {
+        handleNumber(value);
+    }
 }
 
 function handleClear(){
+    currentInput = "0";
+    previousValue = null;
+    operator = null;
+
+    updateDisplay();
+}
+
+function handleDelete() {
+    if (currentInput.length > 1) {
+        currentInput = currentInput.slice(0, -1);
+    } else {
+        currentInput = "0";
+    }
     
+
+    updateDisplay();
+}
+
+
+function updateDisplay() {
+    operationDisplay.innerHTML = currentInput;
+    totalDisplay.innerHTML = previousValue ?? "0";
 }
 
 // document.querySelectorAll(".btn").forEach(btn =>{
